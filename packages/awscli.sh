98d8c8adb3998e14a3952154f0e44ab898930160
JB Brown <jb@JBs-MacBook-Pro.domain>
2017-10-07 20:51:01 -0700

    aws install via bundle

---
+++ packages/awscli.sh
@@ -0,0 +1,6 @@
+mkdir awscli
+cd awscli
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+cd ..
