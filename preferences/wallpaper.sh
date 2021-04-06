a5b70f314c89431eed7f9f77768c3b7735bd4f45
jb-brown <jb@controlstatements.com>
2021-04-06 07:40:46 -0700

    comment out wallpaper setting

--- preferences/wallpaper.sh
+++ preferences/wallpaper.sh
@@ -5,19 +5,19 @@
 # Set wallpaper and login background                                          #
 ###############################################################################
 
-dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-osascript <<END
-	tell application "System Events"
-    set desktopCount to count of desktops
-    repeat with desktopNumber from 1 to desktopCount
-      tell desktop desktopNumber
-        set picture to "$dir/wallpaper.png"
-      end tell
-    end repeat
-  end tell
-END
+#osascript <<END
+#	tell application "System Events"
+#    set desktopCount to count of desktops
+#    repeat with desktopNumber from 1 to desktopCount
+#      tell desktop desktopNumber
+#        set picture to "$dir/wallpaper.png"
+#      end tell
+#    end repeat
+#  end tell
+#END
 
 
 #Change login screen background
-sudo defaults write /Library/Preferences/com.apple.loginwindow DesktopPicture "$dir/wallpaper.png"
+#sudo defaults write /Library/Preferences/com.apple.loginwindow DesktopPicture "$dir/wallpaper.png"
